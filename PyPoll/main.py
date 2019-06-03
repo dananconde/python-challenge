93e0283c0e74559e8ea0b7b2d8409f3807164c9a
Daniel <dconde@alumni.scu.edu>
2019-06-03 12:34:13 -0700

    worked on PyPoll

---
+++ PyPoll/main.py
@@ -0,0 +1,38 @@
+# Dependencies
+
+import os
+import csv
+
+electiondata_csv = os.path.join('..', 'Resources', '03-Python_Homework_Instructions_PyPoll_Resources_election_data.csv')
+
+# Read in CSV file
+
+with open(electiondata_csv, 'r') as csvfile:
+    
+    # Split the data on commas
+
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    header = next(csvreader)
+
+    # Reading each row
+
+    count = 0
+    candidate_vote = []
+    candidate_list = [] 
+      
+    for row in csvreader:
+
+        count += 1
+        candidate_vote.append(row[1])
+
+    for x in candidate_vote: 
+
+        if x not in candidate_list:
+
+            candidate_list.append(x)
+
+
+    # Print Statements
+    print(count)
+    
